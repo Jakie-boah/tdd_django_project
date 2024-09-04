6f2626a578def7253af0eb5bf3927ec1804e17ae
slonn <vanogalen@yandex.ru>
2024-09-04 16:35:40 +0300

    функциональные тесты для разных ссылок списков задач

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,6 +56,60 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail("закончить тест!")
 
+    def test_can_start_a_list_for_one_user(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(by=By.TAG_NAME, value="h1").text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element(by=By.ID, value="id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Купить павлиньи перья")
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Купить павлиньи перья")
+
+        inputbox = self.browser.find_element(by=By.ID, value="id_new_item")
+        inputbox.send_keys("Сделать мушку из павлиньих перьев")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table("1: Купить павлиньи перья")
+        self.wait_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
+
+        self.fail("закончить тест!")
+
+    def test_multiple_users_can_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(by=By.ID, value="id_new_item")
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Safari()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(by=By.TAG_NAME, value="body").text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        inputbox = self.browser.find_element(by=By.ID, value="id_new_item")
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        francis_lit_url = self.browser.current_url
+        self.assertRegex(francis_lit_url, '/lists/.+')
+        self.assertNotEqual(francis_lit_url, edith_list_url)
+
+        page_text = self.browser.find_element(by=By.TAG_NAME, value="body").text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
