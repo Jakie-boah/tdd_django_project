11d32d00082ba37b9c7bfb189d2c5d99d76ce4bf
slonn <vanogalen@yandex.ru>
2024-09-04 16:07:42 +0300

    избавился от time sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 import unittest
 
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -16,10 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(by=By.ID, value="id_list_table")
-        rows = table.find_elements(by=By.TAG_NAME, value="tr")
-        self.assertIn(row_text, [row.text.strip() for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(by=By.ID, value="id_list_table")
+                rows = table.find_elements(by=By.TAG_NAME, value="tr")
+                self.assertIn(row_text, [row.text.strip() for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -33,16 +45,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("Купить павлиньи перья")
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.wait_for_row_in_list_table("1: Купить павлиньи перья")
 
         inputbox = self.browser.find_element(by=By.ID, value="id_new_item")
         inputbox.send_keys("Сделать мушку из павлиньих перьев")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table("1: Купить павлиньи перья")
-        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
+        self.wait_for_row_in_list_table("1: Купить павлиньи перья")
+        self.wait_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
 
         self.fail("закончить тест!")
 
