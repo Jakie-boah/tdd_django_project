ce7308d0c73e7e8986415f89b89ef9d875f8c6ca
slonn <vanogalen@yandex.ru>
2024-09-14 14:26:28 +0300

    разделенные модульные тесты

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,30 +10,6 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item.objects.create(text="The first (ever) list item", list=list_)
-        second_item = Item.objects.create(text="Item the second", list=list_)
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
     """тест представления списка"""
 
